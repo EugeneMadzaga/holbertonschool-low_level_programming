6fe59efb9fb4aed30be2ce78c9d2422393dfd967
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-02-03 11:05:49 +0200

    update c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-
+/**main entry - return 0 **/
 int main(void){
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return(0);
 }
