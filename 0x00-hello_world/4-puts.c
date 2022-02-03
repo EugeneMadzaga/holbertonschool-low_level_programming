7a1ad6fb1e54e35e7e6b4282df6c966e1ac428d9
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-02-03 11:11:55 +0200

    fix c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**main entry - return 0 **/
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle\n");
 return(0);
 }
