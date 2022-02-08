f02cf1e38817978f131f0a84dc35689c22e01016
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:21:56 +0000

    fix print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,7 +1,11 @@
-#include "_putchar"
-#include "6-abs.c"
+#include "main.h"
 #include <stdio.h>
-
+/**
+*print_to_98 - Prints all natural numbers from n to 98
+*@n: The number to start printing from
+*
+*Return: Always 0.
+*/
 void print_to_98(int n)
 {
   int i;
