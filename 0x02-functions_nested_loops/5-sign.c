08267dfee229366f7a53190097b5bd0d977efd33
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 11:31:12 +0000

    fix sign file

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,4 @@
-#include "_putchar"
+#include "main.h"
 /**
  * print_sign - main function
  *
@@ -10,17 +10,18 @@
 
 int print_sign(int n)
 {
-  if (n > 0)
-  {
-    _putchar('+');
-    return (1);
-   }
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
   
-   if (n == 0)
-   {
-    _putchar('0');
-    return (0);
-	}
-    _putchar('-');
-    return (-1);
+if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+
+_putchar('-');
+return (-1);
 }
