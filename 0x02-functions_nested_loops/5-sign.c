7dd7fe91c85aa34b632e5c6e57e10403c3e224bc
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 11:40:49 +0000

    fix sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
- * print_sign - main function
+ *print_sign - Prints the sign of a number
+ *@n: The number to be checked
  *
- * @n: character to compare
- *
- * Return: 1 if positive, 0 if zero, -1 if negative
+ *1 if positive, 0 if zero, -1 if negative
  *
  */
 int print_sign(int n)
@@ -14,7 +13,7 @@ if (n > 0)
 _putchar('+');
 return (1);
 }
-  
+
 if (n == 0)
 {
 _putchar('0');
