2bc67c28b55433a65c6aeb6f178727a93077bd1f
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 11:46:34 +0000

    fix abs_val

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,17 @@
-#include "_putchar"
+#include "main.h"
 /**
- * _abs - main function
- *
- * @n: integer to make absolute
- * Return: int or int times negative
- *
- */
-
+*_abs - main function
+*@c: The number to be computed
+*
+*Return: zero value
+*
+*/
 int _abs(int n)
 {
-  if (n > 0)
-    return (n);
-  else
-    return (n * -1);
+
+if (n > 0)
+return (n);
+else
+return (n * -1);
+
 }
