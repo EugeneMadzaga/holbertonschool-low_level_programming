801794a1cb3149310c54e7accb3a0c1996c474ee
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:07:01 +0000

    update times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,37 +1,38 @@
-#include "_putchar"
-
+#include "main.h"
 /**
- * times_table - times table function
- *
- */
+*times_table - times table function
+*
+*Return: void
+*/
 void times_table(void)
 {
-  int i;
-  int j;
 
-  for (i = 0; i <= 9; i++)
-  {
-  for (j = 0; j <= 9; j++)
-  {
-  int prod = j * i;
+int i;
+int j;
 
-  if (j == 0)
-  {
-    _putchar('0');
-  } else if (prod <= 9)
-  {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(' ');
-    _putchar(prod + '0');
-  } else
-  {
-    _putchar(',');
-    _putchar(' ');
-    _putchar(prod / 10 + '0');
-    _putchar(prod % 10 + '0');
-  }
-  }
-    _putchar('\n');
-  }
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+int prod = j * i;
+
+if (j == 0)
+{
+_putchar('0');
+} else if (prod <= 9)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(prod + '0');
+} else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(prod / 10 + '0');
+_putchar(prod % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
