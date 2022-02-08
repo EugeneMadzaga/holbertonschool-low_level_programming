ce8ba3d26d10835b95e020fcdc2e6e223e9cdc3c
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:15:09 +0000

    fix a+b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,12 @@
-#include "_putchar"
+#include "main.h"
 /**
- * add - adding it up
- * @a: first integer
- * @b: second integer
- *
- * Return: sum of both integers
- */
+* add - adding it up
+* @a: first integer
+* @b: second integer
+*
+* Return: sum of both integers
+*/
 int add(int a, int b)
 {
-	return (a + b);
+return (a + b);
 }
