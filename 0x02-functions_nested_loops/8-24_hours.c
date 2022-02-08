8dc5c4264d926c15368186070c1cb4205b668995
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:00:46 +0000

    8 - 24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,23 +1,25 @@
-#include "_putchar"
+#include "main.h"
 /**
- * jack_bauer - main thing
- *
- */
+*jack_bauer - main thing
+*
+*Return: void
+*/
 void jack_bauer(void)
 {
-  int i;
-  int j;
 
-  for (i = 0; i <= 23; i++)
-  {
-  for (j = 0; j <= 59; j++)
-  {
-    _putchar(i / 10 + '0');
-    _putchar(i % 10 + '0');
-    _putchar(':');
-    _putchar(j / 10 + '0');
-    _putchar(j % 10 + '0');
-    _putchar('\n');
-   }
-   }
+int i;
+int j;
+
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; j <= 59; j++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(j / 10 + '0');
+_putchar(j % 10 + '0');
+_putchar('\n');
+}
+}
 }
