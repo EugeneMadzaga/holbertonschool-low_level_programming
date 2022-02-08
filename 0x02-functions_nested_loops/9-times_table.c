6e9c4d9c1ac984ce77cfccfdaa487f2b9204f544
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:12:59 +0000

    update times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,38 +1,42 @@
 #include "main.h"
 /**
-*times_table - times table function
+*times_table - Prints the 9 times table, starting with 0
 *
-*Return: void
+*Return: empty output
 */
 void times_table(void)
 {
+int x, y, z, u, d;
 
-int i;
-int j;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
 
-for (i = 0; i <= 9; i++)
+if (z > 9)
 {
-for (j = 0; j <= 9; j++)
-{
-int prod = j * i;
+u = z % 10;
+d = (z - u) / 10;
 
-if (j == 0)
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
 {
-_putchar('0');
-} else if (prod <= 9)
+if (y != 0)
 {
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(prod + '0');
-} else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(prod / 10 + '0');
-_putchar(prod % 10 + '0');
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+
+_putchar(z + '0');
 }
 }
+
 _putchar('\n');
 }
 }
