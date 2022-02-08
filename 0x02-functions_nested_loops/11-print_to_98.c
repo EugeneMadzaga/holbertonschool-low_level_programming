d3bd7f8157adefcc3bf3e8d803a9172a954a9b53
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-08 12:25:42 +0000

    fix print to 98 01

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *print_to_98 - Prints all natural numbers from n to 98
 *@n: The number to start printing from
@@ -7,28 +8,28 @@
 */
 void print_to_98(int n)
 {
-  int i;
+int i;
 
-  if (n > 98)
-  {
-  for (i = n; i >= 98; i--)
-  {
-    printf("%d", i);
-  if (i != 98)
-  {
-    printf(", ");
-  }
-  }
-  } else
-  {
-  for (i = n; i <= 98; i++)
-  {
-  printf("%d", i);
-  if (i != 98)
-  {
-    printf(", ");
-  }
-  }
-  }
-    printf("\n");
+if (n > 98)
+{
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+} else
+{
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
 }
