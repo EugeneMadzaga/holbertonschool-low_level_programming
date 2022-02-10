fb33d08b8b6b1d19da1cf2b463eda7b972f708fa
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-10 11:48:39 +0000

    commit mul file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *mul - Multiply 2 integers
+ *@a: first vaue to be multiplied
+ *@b: second value to be multiplied
+ *
+ *Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}
