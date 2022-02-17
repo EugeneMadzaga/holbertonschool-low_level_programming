2ff09f28836aa6e97bb7fd39d082a0ba87d8eb79
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-17 11:50:25 +0000

    fix  5-string_toupper.c header

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,17 +6,16 @@
  * Return: the pointer to dest.
  */
 
-char *string_toupper(char *str)
+char *string_toupper(char *s)
 {
-int index = 0;
+	int count = 0;
 
-while (str[index])
-{
-if (str[index] >= 'a' && str[index] <= 'z')
-str[index] -= 32;
+	while (*(s + count) != '\0')
+	{
+		if ((*(s + count) >= 97) && (*(s + count) <= 122))
+			*(s + count) = *(s + count) - 32;
+		count++;
+	}
 
-index++;
-}
-
-return (str);
+	return (s);
 }
