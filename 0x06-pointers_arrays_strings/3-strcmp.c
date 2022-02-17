37b297e41400880fa52617b8409d4924c08213bb
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-17 11:36:50 +0000

    fix 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,11 +9,15 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2 && *s1 == *s2)
-{
-s1++;
-s2++;
-}
+	int i = 0, op = 0;
 
-return (*s1 - *s2);
+	while (op == 0)
+	{
+		if ((*(s1 + i) == '\0') && (*(s2 + i) == '\0'))
+			break;
+		op = *(s1 + i) - *(s2 + i);
+		i++;
+	}
+
+	return (op);
 }
