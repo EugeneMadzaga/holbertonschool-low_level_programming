763b6384a2985d7815a66c8b4027c9b0d8f63fe4
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-17 11:38:33 +0000

    fix 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- *_strcmp - Compare two strings
- *@s1: First string to be compared
- *@s2: Second string to be compared
- *
- *return: Return first string & second string
+ * _strcmp - Compare two strings
+ * @s1: First string to be compared
+ * @s2: Second string to be compared
+ * Return: 0 if s1 and s2 are equals,
+ * another number if not.
  */
 int _strcmp(char *s1, char *s2)
 {
