ed14918a09550d059f34924444231a3aeea450c2
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-17 11:30:13 +0000

    fix 2-strncpy.c file

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
+
 /**
  * _strncpy - Copy string
  * @dest: Pointer to be appended, array that returns
  * @scr: Source value
  * @n: Number of copy limit
- *
  * Return: char value
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 int a = 0, b = 0;
