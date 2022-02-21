0c1d9a289a9e90b1d2209cc76b84e758de2e5511
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-21 11:23:38 +0000

    quick fix memcpy file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,18 +1,17 @@
-#include "main.h"
 /**
- * _memcpy - Copy memory area
+ * _memcpy - copies from source to dest values in array
  * @dest: destination to copy to
- * @src: Source to copy from
- * @n: Amount of times
- * Return: The string value
+ * @src: source to copy from
+ * @n: n amount of times
+ * Return: return the string value
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-  unsigned int i;
+	unsigned int i;
 
-  for (i = 0; i < n; i++)
-    {
-      dest[i] = src[i]
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
 	}
-  return (dest);
+	return (dest);
 }
