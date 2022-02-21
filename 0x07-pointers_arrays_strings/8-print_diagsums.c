cad359e80cd760cc12d1bfb6f4b4c6582d5fb8b5
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-21 11:45:16 +0000

    fix whitespaces print_diagsums

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,14 +8,14 @@
  */
 void print_diagsums(int *a, int size)
 {
-  int i;
-  int diagO = 0;
-  int diagT = 0;
+int i;
+int diagO = 0;
+int diagT = 0;
 
-  for (i = 0; i < size; i++)
-    {
-          diagO += a[(i * size) + i];
-          diagT += a[(size - 1) + ((size - 1) * i)];
-    }
-    printf("%d, %d\n", diagO, diagT);
+for (i = 0; i < size; i++)
+  {
+    diagO += a[(i * size) + i];
+    diagT += a[(size - 1) + ((size - 1) * i)];
+  }
+ printf("%d, %d\n", diagO, diagT);
 }
