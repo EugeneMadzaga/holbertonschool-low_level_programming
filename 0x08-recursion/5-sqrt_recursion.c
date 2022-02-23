cd242134121f62f800fc9e6e92d657743c67392f
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-23 09:55:11 +0000

    fix 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -7,8 +7,8 @@
  *         If n does not have a natural square root - -1.
  */
 int find_sqrt(int num, int root);
-int _sqrt_recursion(int n);
-
+int _sqrt_recursion(int n)
+	
 /**
  * find_sqrt - Finds the natural square root of an inputted number.
  * @num: The number to find the square root of.
