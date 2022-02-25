b05427e0b4acdcab757062314d1a46d75cec0f82
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-02-25 08:40:47 +0000

    push arg_argv files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Print program name
+ * @argc: The number of arguments
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
