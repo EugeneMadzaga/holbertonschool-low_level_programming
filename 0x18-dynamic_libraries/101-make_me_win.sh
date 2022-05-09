e2cdd11b127c82222ca2452c508ca9f341f469f4
EugeneMadzaga <eugenemadzaga@gmail.com>
2022-05-09 22:58:29 +0200

    update make win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/EugeneMadzaga/holbertonschool-low_level_programming/blob/main/0x18-dynamic_libraries/libdynamic.so
-export LD_PRELOAD="$PWD/../libdynamic.so"
+wget -P .. https://github.com/EugeneMadzaga/holbertonschool-low_level_programming/blob/18d5f3f6b52d5920363af01cf89b5925976e93e1/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
