09c96857b230f4f4ff1fcc8d13d7d40f3a2fd910
Singular Learner 4 <SSLearner4@SINGULAR.CO.ZA>
2022-05-10 08:32:01 +0100

    fix

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P .. https://github.com/EugeneMadzaga/holbertonschool-low_level_programming/blob/e13deecbe00c7211c5aa8c79542ee18344baa811/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD libgiga"
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
